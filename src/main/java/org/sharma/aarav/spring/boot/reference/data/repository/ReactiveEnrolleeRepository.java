5f9e6e5f5b71337a41b2447016929ff8e6d6040d
aaravsharma <aaravsharma927@gmail.com>
2021-03-01 04:13:52 -0800

    Created a reactive EnrolleeRepository by extending ReactiveCrudRepository.

---
+++ src/main/java/org/sharma/aarav/spring/boot/reference/data/repository/ReactiveEnrolleeRepository.java
@@ -0,0 +1,16 @@
+package org.sharma.aarav.spring.boot.reference.data.repository;
+
+import org.sharma.aarav.spring.boot.reference.data.model.Enrollee;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ReactiveEnrolleeRepository extends ReactiveCrudRepository<Enrollee, String> {
+
+    Mono<Enrollee> findByEnrolleeId(String enrolleeId);
+
+    Mono<Enrollee> findByPersonId(String personId);
+
+    Flux<Enrollee> findByEnrolleeName(String enrolleeName);
+
+}
