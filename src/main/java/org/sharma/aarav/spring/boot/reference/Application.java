cbcbb7de5f09e00aaa3026e43f84d2ac69b3d994
aaravsharma <aaravsharma927@gmail.com>
2021-03-06 20:43:16 -0800

    Modify Application.java main SpringBootApplication class

--- src/main/java/org/sharma/aarav/spring/boot/reference/Application.java
+++ src/main/java/org/sharma/aarav/spring/boot/reference/Application.java
@@ -1,11 +1,20 @@
 package org.sharma.aarav.spring.boot.reference;
 
+import org.sharma.aarav.spring.boot.reference.data.config.EmbeddedReactiveMongoConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class , MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@ComponentScan(basePackages = {"org.sharma.aarav.spring.boot.reference.*"})
+@AutoConfigureBefore(EmbeddedReactiveMongoConfig.class)
+//@Import(EmbeddedReactiveMongoConfig.class)
 public class Application {
 
     private static final Logger logger = LoggerFactory
