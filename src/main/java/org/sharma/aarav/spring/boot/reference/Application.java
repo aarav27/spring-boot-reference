8627fbe282dc5f9aaca2167db22d844024c5c748
aarav27 <aaravsharma927@gmail.com>
2021-03-29 18:04:39 -0700

    Added Author

--- src/main/java/org/sharma/aarav/spring/boot/reference/Application.java
+++ src/main/java/org/sharma/aarav/spring/boot/reference/Application.java
@@ -1,5 +1,9 @@
 package org.sharma.aarav.spring.boot.reference;
 
+/**
+ * @author Aarav Sharma
+ */
+
 import org.sharma.aarav.spring.boot.reference.data.config.EmbeddedReactiveMongoConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
